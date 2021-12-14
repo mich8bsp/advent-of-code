e741ccd9927696b87fae74e43b636e38c8efbc37
Michael Bespalov <mich8bsp@gmail.com>
2021-12-14 18:54:39 +0200

    Advent of Code - Day 14

---
+++ src/main/scala/aoc2021/Day14.scala
@@ -0,0 +1,79 @@
+package aoc2021
+
+import scala.io.Source
+
+object Day14 {
+
+  def producePolymer(template: String, rules: List[(String, Char)], iterations: Int): Map[Char, Long] = {
+    val rulesMap: Map[String, Char] = rules.toMap
+
+    def inner(pairsOccurrences: Map[(Char, Char), Long], iterationsLeft: Int): Map[(Char, Char), Long] = {
+      if(iterationsLeft == 0){
+        pairsOccurrences
+      }else {
+        val pairsOccurrencesAfterOneIteration: Map[(Char, Char), Long] = pairsOccurrences.toSeq.flatMap({
+          case (pair, numOfOccurrences) =>
+            val pairAsStr: String = s"${pair._1}${pair._2}"
+            rulesMap.get(pairAsStr)
+              .map(insertionChar => {
+                Seq((pair._1, insertionChar) -> numOfOccurrences, (insertionChar, pair._2) -> numOfOccurrences)
+              }).getOrElse(Seq(pair -> numOfOccurrences))
+        }).groupBy(_._1)
+          .view
+          .mapValues(_.map(_._2).sum)
+          .toMap
+
+        inner(pairsOccurrencesAfterOneIteration, iterationsLeft - 1)
+      }
+    }
+
+    val pairs = (0 until template.length-1).map(i => (template(i), template(i+1)))
+      .groupBy(identity)
+      .view
+      .mapValues(_.size.toLong)
+      .toMap
+
+    val charToNumOfOccurrencesInPairs = inner(pairs, iterations).toSeq.flatMap({
+      case ((start, end), num) => Seq(start -> num, end -> num)
+    })
+
+    charToNumOfOccurrencesInPairs.groupBy(_._1).map({
+      case (c, occurrences) =>
+        val sumOfOccurrences = occurrences.map(_._2).sum
+        (c, (sumOfOccurrences + sumOfOccurrences % 2)/2)
+    })
+  }
+
+  def getPolymerFrequencyDiff(charFrequency: Map[Char, Long]): Long = {
+    charFrequency.values.max - charFrequency.values.min
+  }
+
+  def parseInput(filePath: String): (String, List[(String, Char)]) = {
+    val lines = Source.fromResource(filePath).getLines().toList
+
+    val template = lines.head
+    val insertionRules = lines.filter(_.contains("->"))
+      .map(line => {
+        val Array(from, to) = line.split("->").map(_.trim)
+        (from, to.toCharArray.head)
+      })
+
+    (template, insertionRules)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val (templateTest, rulesTest) = parseInput("input_2021_14_test.txt")
+
+    val testPolymer10 = producePolymer(templateTest, rulesTest, 10)
+    val testPolymer40 = producePolymer(templateTest, rulesTest, 40)
+    println(getPolymerFrequencyDiff(testPolymer10))
+    println(getPolymerFrequencyDiff(testPolymer40))
+
+    val (template, rules) = parseInput("input_2021_14.txt")
+
+    val polymer10 = producePolymer(template, rules, 10)
+    val polymer40 = producePolymer(template, rules, 40)
+    println(getPolymerFrequencyDiff(polymer10))
+    println(getPolymerFrequencyDiff(polymer40))
+  }
+}
