46acb4bff8f7a01aa3c0cde6282d86f82f8aff34
Michael Bespalov <michael@genoox.com>
2022-11-11 17:32:30 +0200

    2020 day4

---
+++ src/main/scala/aoc2020/Day4.scala
@@ -0,0 +1,66 @@
+package aoc2020
+
+import scala.collection.mutable
+import scala.util.Try
+
+object Day4 {
+  val mandatoryPassportFields = Set("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+  case class Passport(fields: Map[String, String])
+
+  def parsePassports(lines: List[String]): List[Passport] = {
+    def parsePassport(passportLines: List[String]): Passport = {
+      Passport(passportLines.flatMap(_.split(" "))
+        .filter(_.nonEmpty)
+        .map(line => (line.split(":").head, line.split(":")(1)))
+        .toMap)
+    }
+
+    val passports = mutable.Buffer[Passport]()
+    var linesLeft = lines
+    while (linesLeft.nonEmpty) {
+      val currPassportLines = linesLeft.takeWhile(_.trim.nonEmpty)
+      passports.append(parsePassport(currPassportLines))
+      linesLeft = linesLeft.dropWhile(_.trim.nonEmpty)
+      if (linesLeft.nonEmpty) {
+        linesLeft = linesLeft.tail
+      }
+    }
+
+    passports.toList
+  }
+
+  def solveA(passports: List[Passport]): Int = {
+    val validator: Passport => Boolean = (p: Passport) => mandatoryPassportFields.subsetOf(p.fields.keySet)
+
+    passports.count(validator)
+  }
+
+  def solveB(passports: List[Passport]): Int = {
+    val validator: Passport => Boolean = (p: Passport) => {
+      lazy val allFieldsPresent = mandatoryPassportFields.subsetOf(p.fields.keySet)
+
+      lazy val fieldsValid = p.fields.map {
+        case ("byr", v) => Try(v.toInt).filter((1920 to 2002).contains).isSuccess
+        case ("iyr", v) => Try(v.toInt).filter((2010 to 2020).contains).isSuccess
+        case ("eyr", v) => Try(v.toInt).filter((2020 to 2030).contains).isSuccess
+        case ("hgt", v) =>
+          (v.endsWith("cm") && Try(v.dropRight(2).toInt).filter((150 to 193).contains).isSuccess) ||
+          (v.endsWith("in") && Try(v.dropRight(2).toInt).filter((59 to 76).contains).isSuccess)
+        case ("hcl", v) => v.length == 7 && v.startsWith("#") && v.tail.matches("[0-9a-f]+")
+        case ("ecl", v) => Set("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(v)
+        case ("pid", v) => v.length == 9 && Try(v.toLong).isSuccess
+        case _ => true
+      }.reduce(_ && _)
+
+      allFieldsPresent && fieldsValid
+    }
+
+    passports.count(validator)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val passports = parsePassports(readFileLines[String](4))
+    println(solveA(passports))
+    println(solveB(passports))
+  }
+}
