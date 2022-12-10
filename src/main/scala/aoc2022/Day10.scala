c402e24433d5adfc0ab06ec84b56a7b95d7db5a7
michaelb <mich8bsp@gmail.com>
2022-12-10 15:20:38 +0200

    aoc 2022 day 10

--- src/main/scala/aoc2022/Day10.scala
+++ src/main/scala/aoc2022/Day10.scala
@@ -15,10 +15,10 @@ object Day10 {
   def runCycle(instructions: List[Instruction], register: Int): (List[Instruction], Int) = instructions match {
       case Nil => (Nil, register)
       case Noop :: xs => (xs, register)
-      case AddX(n, cyclesLeft) :: xs if cyclesLeft > 1 =>
-        (AddX(n, cyclesLeft - 1) :: xs, register)
       case AddX(n, 1) :: xs =>
         (xs, register + n)
+      case AddX(n, cyclesLeft) :: xs =>
+        (AddX(n, cyclesLeft - 1) :: xs, register)
     }
 
   @tailrec
@@ -39,14 +39,16 @@ object Day10 {
 
     @tailrec
     def render(instructionsLeft: List[Instruction], cycle: Int = 1, register: Int = 1, renderIdx: Int = 0): Unit = {
-      val spritePosition: Seq[Int] = (register  - 1 to register + 1).map(_ % 40)
-      val pixel: Char = if (spritePosition.contains(renderIdx % 40)) '#' else '.'
       val (nextCycleInstructions: List[Instruction], updatedRegister: Int) = if (instructionsLeft.nonEmpty) {
         runCycle(instructionsLeft, register)
       } else {
         (Nil, register)
       }
-      crt(renderIdx / cols)(renderIdx % cols) = pixel
+
+      val spritePosition: Seq[Int] = (register  - 1 to register + 1).map(_ % cols)
+      if (spritePosition.contains(renderIdx % cols)) {
+        crt(renderIdx / cols)(renderIdx % cols) = '#'
+      }
 
       if (renderIdx < rows * cols - 1) {
         render(nextCycleInstructions, cycle + 1, updatedRegister, renderIdx + 1)
