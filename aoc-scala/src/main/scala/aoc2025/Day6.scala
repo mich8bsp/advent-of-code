2fe35573d8b87d737529215bf0fd0118f0d4809d
Michael Bespalov <mich8bsp@gmail.com>
2026-01-03 09:39:09 +0200

    day6

--- aoc-scala/src/main/scala/aoc2025/Day6.scala
+++ aoc-scala/src/main/scala/aoc2025/Day6.scala
@@ -9,6 +9,36 @@ object Day6 {
     }.sum
   }
 
+  def parseSections(lines: List[String]): Seq[List[String]] = {
+    val separationCols = (0 until lines.head.length).filter { i =>
+      lines.map(_(i)).toSet == Set(' ')
+    }
+    val sectionStartEnd = List((0, separationCols.head)) ++ separationCols.sliding(2).map {
+      twoCols => (twoCols.head + 1, twoCols.last)
+    }.toList ++ List((separationCols.last + 1, lines.map(_.length).max))
+
+    val sections: Seq[List[String]] = sectionStartEnd.map {
+      case (start, end) =>
+        lines.map(_.slice(start, end))
+    }
+
+    sections
+  }
+
+  def solve2(sections: Seq[List[String]], ops: Array[String]): Long = {
+    sections.zip(ops).map {
+      case (section, op) =>
+        (0 until section.map(_.length).max).map { i =>
+          section.map(line => if (i >= line.length) "" else line(i) ).mkString.trim.toLong
+        }.reduce { (a: Long, b: Long) =>
+         op match {
+            case "+" => a + b
+            case "*" => a * b
+          }
+        }
+    }.sum
+  }
+
   def main(args: Array[String]): Unit = {
     val lines = readFileLines(6)(identity)
     val problems = lines.dropRight(1).map { line =>
@@ -16,5 +46,8 @@ object Day6 {
     }
     val ops = lines.last.split(" ").filter(_.nonEmpty)
     println(solve(problems, ops))
+
+    val sections = parseSections(lines.dropRight(1))
+    println(solve2(sections, ops))
   }
 }
